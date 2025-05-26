908f0e717cd3df0c78133f86d3f3bbe1490e236e
baileycschroder <baileycschroder@gmail.com>
2025-05-26 09:00:04 -0700

    Add random greeting generator function

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,19 @@
 print("Hello, World!")
 print("This is my first Git commit!")
 print("programming isn't about what you know, it's about what you can figure out.")
+import random
+
+greetings = [
+    "Hello, Git!",
+    "Greetings, developer!",
+    "Welcome to branching!",
+    "Hi there, coding friend!",
+    "Happy coding!"
+]
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+print(get_random_greeting())
+print("Learning about branches today!")
+
